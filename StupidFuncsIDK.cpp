88fd09d7d028519983ecf15d876539e5d894a55a
Judah <67433376+judahmane@users.noreply.github.com>
2021-09-03 21:16:43 -0400

    Update StupidFuncsIDK.cpp

--- StupidFuncsIDK.cpp
+++ StupidFuncsIDK.cpp
@@ -34,6 +34,35 @@ void getkeyinput()
 		}
 	}
 }
+void keyinputnice(std::string key)
+{
+	bool keyfart = true;
+	
+	std::cout << "Enter Key To Spam And Display\n";
+	std::cin >> key;
+	if (key == "A")
+	{
+		while (keyfart = true)
+		{
+			if (GetAsyncKeyState(VK_A))
+			{
+				Sleep(1);
+				std::cout << "A\n";
+			}
+		}
+	}  //pretty sure you can use this and reinterpt it to make a control settings code for a game or something like a reference to the function.
+	else if (key == "D")
+	{
+		while (keyfart = true)
+		{
+			if (GetAsyncKeyState(VK_D))
+			{
+				Sleep(1);
+				std::cout << "D\n";
+			}
+		}
+	}
+}
 
 int main()
 {
